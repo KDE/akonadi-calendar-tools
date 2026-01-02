b53cf53d9986c2c1e008d4232c4621e313589f50
Laurent Montel <montel@kde.org>
2026-01-02 15:54:14 +0100

    Fix the Qt commercial license naming

--- konsolekalendar/konsolekalendarvariables.h
+++ konsolekalendar/konsolekalendarvariables.h
@@ -5,7 +5,7 @@
  * SPDX-FileCopyrightText: 2002-2004 Tuukka Pasanen <illuusio@mailcity.com>   *
  * SPDX-FileCopyrightText: 2003-2005 Allen Winter <winter@kde.org>            *
  *                                                                            *
- * SPDX-License-Identifier: GPL-2.0-or-later WITH Qt-Commercial-exception-1.0 *
+ * SPDX-License-Identifier: GPL-2.0-or-later WITH LicenseRef-Qt-Commercial-exception-1.0 *
  *                                                                            *
  *****************************************************************************/
 
